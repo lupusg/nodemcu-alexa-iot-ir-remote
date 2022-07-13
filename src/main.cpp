69495033c1d71062ce4c2ed8f094bd938fd47e01
lupusg <just_vlad@yahoo.com>
2022-07-13 16:48:45 +0300

    Add simple turn off command for Mitsubishi AC

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+#include <ir_Mitsubishi.h>
+
+const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+IRMitsubishiAC remote(kIrLed);
 
 void setup() {
-// write your initialization code here
+  Serial.begin(115200);
+  remote.begin();
+  remote.setFan(0);
+  remote.setMode(kMitsubishi112Cool);
+  remote.setTemp(24);
+  remote.setVane(kMitsubishiAcVaneAuto);
+  delay(200);
 }
 
 void loop() {
-// write your code here
+  remote.off();
+  remote.send();
+  delay(2000);
 }
