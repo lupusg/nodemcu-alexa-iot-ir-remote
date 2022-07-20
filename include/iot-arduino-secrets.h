1d4c7d9154179f347fc3763ae571ab09bb70bca9
lupusg <just_vlad@yahoo.com>
2022-07-21 00:53:18 +0300

    Add include guard

---
+++ include/iot-arduino-secrets.h
@@ -0,0 +1,18 @@
+/**
+ * @file iot-arduino-secrets.h
+ *
+ * @brief Arduino IOT configuration file that contains secrets for the device.
+ *
+ * @date July 14, 2022
+ *
+ * @author Vlad-Marian Lupu
+ */
+
+#ifndef NODEMCU_ALEXA_IOT_IR_REMOTE_INCLUDE_IOT_ARDUINO_SECRETS_H_
+#define NODEMCU_ALEXA_IOT_IR_REMOTE_INCLUDE_IOT_ARDUINO_SECRETS_H_
+
+#define SECRET_SSID "Home 2.4G"
+#define SECRET_PASS "nudauparola"
+#define SECRET_DEVICE_KEY "3SWGX8QH56DHUFYPNN7O"
+
+#endif //NODEMCU_ALEXA_IOT_IR_REMOTE_INCLUDE_IOT_ARDUINO_SECRETS_H_
--- include/iot-cloud-connection.h
+++ include/iot-cloud-connection.h
@@ -1,6 +1,9 @@
-#include "iot-arduino-secrets.h"
+#ifndef	NODEMCU_ALEXA_IOT_IR_REMOTE_INCLUDE_IOT_CLOUD_CONNECTION_H_
+#define NODEMCU_ALEXA_IOT_IR_REMOTE_INCLUDE_IOT_CLOUD_CONNECTION_H_
+
 #include <ArduinoIoTCloud.h>
 #include <Arduino_ConnectionHandler.h>
+#include "iot-arduino-secrets.h"
 
 const char kDeviceLoginName[]  = "79e87b3f-6747-44b2-8f50-c0149a3528c1";
 
@@ -23,3 +26,4 @@ void InitIotCloud(){
   ArduinoCloud.printDebugInfo();
 }
 
+#endif //NODEMCU_ALEXA_IOT_IR_REMOTE_INCLUDE_IOT_CLOUD_CONNECTION_H_
