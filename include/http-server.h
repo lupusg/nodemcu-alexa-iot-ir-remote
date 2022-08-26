b54da060307f4a3eca581873d0ac03708d0fe15c
lupusg <just_vlad@yahoo.com>
2022-08-26 23:58:52 +0300

    Add restful api basic auth

--- include/http-server.h
+++ include/http-server.h
@@ -10,6 +10,7 @@
  * @file http-server.h
  *
  * @brief The HTTP server.
+ * 		  API Auth adapted from https://www.mischianti.org/2020/11/17/web-server-on-esp8266-add-secure-rest-back-end-5/
  *
  * @date July 21, 2022
  *
@@ -21,22 +22,29 @@
 
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
-#include "iot-arduino-secrets.h"
+#include <Arduino_DebugUtils.h>
+#include <Hash.h>
+#include "arduino-config.h"
 
-ESP8266WebServer server(80);
+ESP8266WebServer server(WEBSERVER_PORT);
 WiFiClient wifi_client;
 HTTPClient http_client;
 
 const char *api_url = "http://192.168.0.104:8081/signal";
 
+void ServerRouting();
 void HandleOnConnect();
+void HandleToggleReceiving();
 void HandleNotFound();
-void StartReceiving();
+void HandleLogin();
+void HandleLogout();
+
+bool IsAuthenticated();
 
 bool is_receiving = false;
 
 /**
- * The WiFi connection & webserver setup.
+ * WiFi connection & webserver setup.
  */
 void InitHttpServer() {
   Serial.print("Connecting to ");
@@ -52,45 +60,132 @@ void InitHttpServer() {
   Serial.print("AP IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", HandleOnConnect);
-  server.on("/startReceiving", StartReceiving);
-  server.onNotFound(HandleNotFound);
+  ServerRouting();
+  server.collectHeaders("User-Agent", "Cookie");
   server.begin();
   Serial.println("HTTP server started.");
 }
 
 /**
- * The main-loop for the HTTP server.
+ * Main-loop for the HTTP server.
  */
 void HttpServerLoop() {
   server.handleClient();
 }
 
 /**
+ * All server routes.
+ */
+void ServerRouting() {
+  server.on("/login", HTTP_POST, HandleLogin);
+  server.on("/logout", HTTP_GET, HandleLogout);
+
+  server.on("/toggle-receiving", HandleToggleReceiving);
+
+  server.on("/", HandleOnConnect);
+  server.onNotFound(HandleNotFound);
+}
+
+/**
  * The handler for the main page.
  */
 void HandleOnConnect() {
-  server.send(200, "text/plain", "Hello from ESP8266!");
+  if (IsAuthenticated()) {
+	server.send(200, "text/plain", "Hello from ESP8266!");
+  }
 }
 
 /**
  * The handler for the /test endpoint.
  */
-void StartReceiving() {
-  is_receiving = !is_receiving;
-  if (is_receiving) {
-	Serial.println("[HTTP]Receiving IR codes");
-  } else {
-	Serial.println("[HTTP]Stopped receiving IR codes");
+void HandleToggleReceiving() {
+  if (IsAuthenticated()) {
+	is_receiving = !is_receiving;
+	if (is_receiving) {
+	  Serial.println("[HTTP]Receiving IR codes");
+	} else {
+	  Serial.println("[HTTP]Stopped receiving IR codes");
+	}
+	server.send(200, "text/plain", "Toggle successfully");
   }
-  server.send(200, "text/plain", "received successfully");
 }
 
 /**
  * The handler for the 404 not found endpoint.
  */
 void HandleNotFound() {
-  server.send(404, "text/plain", "Not found");
+  server.send(404, "text/plain", "Not found.");
+}
+
+/**
+ * The login handler.
+ * Stores the token 'ESPSESSIONID=sha1(username:password)' as a cookie.
+ */
+void HandleLogin() {
+  Serial.println("Handle login");
+  String message;
+
+  if (server.hasHeader("Cookie")) {
+	Serial.print("Found cookie: ");
+	String cookie = server.header("Cookie");
+	Serial.println(cookie);
+  }
+
+  if (server.hasArg("username") && server.hasArg("password")) {
+	Serial.print("Found parameter: ");
+
+	if (server.arg("username") == String(API_USERNAME) && server.arg("password") == String(API_PASSWORD)) {
+	  server.sendHeader("Location", "/");
+	  server.sendHeader("Cache-Control", "no-cache");
+
+	  String token = sha1(String(API_USERNAME) + ":" + String(API_PASSWORD));
+	  server.sendHeader("Set-Cookie", "ESPSESSIONID=" + token);
+
+	  server.send(301);
+	  Serial.println("Log in Successful");
+	  return;
+	}
+	message = "Wrong username/password! try again.";
+	Serial.println("Log in Failed");
+	server.sendHeader("Location", "/login.html?message=" + message);
+	server.sendHeader("Cache-Control", "no-cache");
+	server.send(301);
+	return;
+  }
+}
+
+/**
+ * The logout handler.
+ */
+void HandleLogout() {
+  Serial.println("Disconnection");
+  server.sendHeader("Location", "/login.html?msg=User disconnected");
+  server.sendHeader("Cache-Control", "no-cache");
+  server.sendHeader("Set-Cookie", "ESPSESSIONID=0");
+  server.send(301);
+}
+
+/**
+ * Checks if the token 'ESPSESSIONID=sha1(username:password' is present in the request cookie.
+ * @return True if it is, false otherwise.
+ */
+bool IsAuthenticated() {
+  Serial.println("Enter is_authenticated");
+  if (server.hasHeader("Cookie")) {
+	Serial.print("Found cookie: ");
+	String cookie = server.header("Cookie");
+	Serial.println(cookie);
+
+	String token = sha1(String(API_USERNAME) + ":" + String(API_PASSWORD));
+
+	if (cookie.indexOf("ESPSESSIONID=" + token) != -1) {
+	  Serial.println("Authentication Successful");
+	  return true;
+	}
+  }
+  Serial.println("Authentication Failed");
+  server.send(401, F("application/json"), "({\"msg\": \"You must authenticate!\"})");
+  return false;
 }
 
 #endif //NODEMCU_ALEXA_IOT_IR_REMOTE_INCLUDE_HTTP_SERVER_H_
